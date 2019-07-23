fb0961309e8e5a3685b546f2c34e3cc09d8533c2
Oliver Tsang <oortsang@uchicago.edu>
2019-07-23 17:53:36 -0500

    New simulation scaling system

--- ToyWells/sim.py
+++ ToyWells/sim.py
@@ -3,46 +3,66 @@ import pdb
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 import h5py
+import pdb
 
 from multipoly import MultiPolynomial
 
-# FF = MultiPolynomial([[13,1,-4,4],[0,-2,0,3]])
-# FF = MultiPolynomial([[0,0,1], [0,1,2],[1,-2, 4]])
-# GG = FF.grad()
-
-
 # Brownian motion / taking random steps
 
-kB = 1.380649e-23 # units of J/K
+# kB = 1.380649e-23 # units of J/K
+# kB = 1.380649e-3 # units of kg ã² / s²
+kB = 1.380649e21 # units of kg ã² / ps²
+tstep = 10 # ps
 
 class Particle():
-    def __init__(self, Pot, pos = 0, DkT = 1, nsize = 1e-2, nsig = 1):
-        self.Pot = Pot           # potential well
-        self.F = -1*Pot.grad()   # force function
-        self.DkT = DkT           # 1/gamma = D/kT
-        self.nsize = nsize       # noise size
-        self.nsig  = nsig        # stdev of gaussian noise
-        self.dim = self.Pot.dim  # dimensionality
-        self.pos = np.array(pos, dtype=np.float64) # ND position
+    def __init__(self, Pot, pos = 0, D = 0.1, T = 300, nsize = 1, dt = tstep):
+        """Initializes the simulation
+
+        Arguments:
+           Pot (MultiPolynomial):    a polynomial describing a potential function
+           pos (np array):           a vector describing the start position
+           D (scalar):               diffusion coefficient in amstrongs^2/ps
+           T (scalar):               temperature in K
+           nsize (scalar):           can magnify the size of noise
+           dt (scalar):              timestep in seconds
+        """
+        self.Pot   = Pot              # potential well
+        self.F     = -1*Pot.grad()    # force function
+        self.DkT   = D/(kB*T)         # 1/gamma = D/kT
+        self.tstep = tstep            # time step
+        self.nsize = nsize            # noise size
+        self.nsig  = np.sqrt(2*D*dt)  # noise standard deviation
+
+        self.dim   = self.Pot.dim     # dimensionality
+        self.pos   = np.array(pos, dtype=np.double) # ND position
         if self.pos.ndim == 0:
             self.pos = self.pos[np.newaxis]
 
-        # could probably make this more efficient
+        # initializing the start position to be robust in
+        # the case of a too-low-dimensional self.pos
         if (self.pos.shape[0] < self.dim):
-            newpos = np.zeros(self.dim, dtype=np.float64)
+            newpos = np.zeros(self.dim, dtype=np.double)
             newpos[:self.pos.shape[0]] = self.pos
             self.pos = newpos
         # self.tot_noise = 0
-        
-    def step(self, dt):
-        # pdb.set_trace()
-        force = self.F(self.pos)
-        noise = self.nsize * np.random.normal(0,self.nsig, self.dim)
-        # self.tot_noise += noise
-        self.v = self.DkT*force + noise
-        self.pos += dt * self.v
 
-# QuadWell = MultiPolynomial([[1,0,-6,0,0],[1,-0.5,-1,1,0]]) # 2 dimensions!
+    def vel(self, pos):
+        force = self.F(pos)
+        noise = self.nsize * np.random.normal(0, self.nsig,  self.dim)
+        return (self.DkT * force + noise)
+        
+    def step(self):
+        # # Euler integration
+        # force = self.F(self.pos)
+        # noise = self.nsize * np.random.normal(0, self.nsig, self.dim)
+        # v = self.DkT*force + noise
+        # self.pos += self.tstep * v
+
+        # Midpoint Integration
+        v1 = self.vel(self.pos) # current velocity
+        k1 = self.tstep * v1 # Euler prediction of motion
+        v2 = self.vel(self.pos + 0.5 * k1) # prediction from midway through the step
+        self.pos += self.tstep * v2
 
 # TriCoeffs = np.array([ [0, 0, 0, 0, 0, 0,   1],   # x^6
 #                        [0, 0, 0, 0, 0, 0, 0.2],     # x^5
@@ -80,25 +100,26 @@ class Particle():
 # SWell = MultiPolynomial([1,        0, -3, 0,  0]) # shallow?
 
 # OldNWell = MultiPolynomial([5,0, -0.1, 0, 0])
+# NewOldNWell = MultiPolynomial([80,0, -0.5, 0, 0]) # narrow (double) well
 
-HWell = MultiPolynomial([             2, 0,  0]) # harmonic
-NWell = MultiPolynomial([80,0, -0.5, 0, 0]) # narrow (double) well
+a = np.array(1e26, dtype=np.double)
+HWell = a*MultiPolynomial([        1, 0, 0]) # harmonic
+NWell = 1e22*MultiPolynomial([8,  0, -300, 0, 0]) # narrow (double) well
 
-xs = np.arange(-0.15, 0.15, 0.005)
+xs = np.arange(-8, 8, 0.01)
 plt.plot(xs, NWell(xs))
 plt.plot(xs, HWell(xs))
 plt.show()
 
 
-p1 = Particle(NWell, nsize = 0.9, DkT = 3.5, nsig = 1.5, pos = 0)
-
-p2 = Particle(HWell, nsize = 0.1, DkT = 1, nsig = 6)
+p1 = Particle(NWell, D = 1e-5, nsize = 1)
+p2 = Particle(HWell, D = 1e-4, nsize = 1)
 
 # # Spit out the coordinates (and control the different trajectories...)
 
-npart = 2
-particles = [p1,
-             p2,
+npart = 1
+particles = [p2,
+             p1,
              # p3,
              # ph2,
              # Particle(0.5*SWell, pos = 0.1, nsize = 5, DkT = 0.1),                 # 4
@@ -106,9 +127,8 @@ particles = [p1,
              # Particle(MultiPolynomial([0.5, 1, 4]), pos = -2, nsize = 1),          # 6
              # Particle(MultiPolynomial([2, 13, 0]), pos = -0.1, nsize = 1),         # 7
             ][:npart]
-tstep = 2e-3
-# nsteps = 105000
 nsteps = 105000
+# nsteps = 205000
 dimensions = 0
 for i in range(npart):
     dimensions += particles[i].dim
@@ -116,12 +136,13 @@ print("Dimensions:", dimensions)
 tracks = np.zeros((nsteps, dimensions), dtype=np.float32)
 for i in tqdm(range(tracks.shape[0])):
     j = 0
+    # pdb.set_trace()
     for p in particles:
-        # pdb.set_trace()
-        p.step(tstep)
+        p.step()
         pdim = p.dim
         tracks[i,j:j+pdim] = p.pos
         j+= pdim
+    # print(("Time %d ps: " % i), tracks[i,:])
 
 if False:
     h5file = h5py.File('data/SimOutput.h5', 'w')
@@ -135,10 +156,9 @@ if False:
 
 print("Fraction of time particle 1 spends on the right of 0:", np.sum(tracks[:,0] >= 0)/tracks.shape[0])
 
-Hist, edges = np.histogram(tracks[:,0], bins = 30)
-plt.plot(Hist)
+plt.plot(np.arange(0,(i+1)*tstep, tstep), tracks)
 plt.show()
 
-
-plt.plot(tracks)
+Hist, edges = np.histogram(tracks[:,0], bins = 30)
+plt.plot(0.5*(edges[1:]+edges[:-1]),Hist)
 plt.show()
